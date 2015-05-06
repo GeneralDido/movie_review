83186a99b6dcaf406a4b0db6651dda14e15bce32
GeneralDido <dimpanouris@gmail.com>
2015-05-06 19:09:03 +0200

    Add boostrap and basic styling

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
   belongs_to(:user)
   
-  has_attached_file :image, styles: { medium: "400x600#>" }
+  has_attached_file :image, styles: { medium: "300x300#>", :small => "150x150#>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
