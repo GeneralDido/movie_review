b0da4d7f5f2adde482de555c6ced124f294fb1d8
GeneralDido <dimpanouris@gmail.com>
2015-05-07 16:19:16 +0200

    Added reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
-  belongs_to(:user)
+  belongs_to :user
+  has_many :reviews
   
-  has_attached_file :image, styles: { medium: "300x300#>", :small => "150x150#>" }
+  has_attached_file :image, styles: { medium: "300x300#", :small => "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
